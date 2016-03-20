c2f30d50bdd88a90889a243ad217154d0da2819f
pihme <pihme@users.noreply.github.com>
2016-03-20 21:02:39 +0100

    popuplate db, improve index and show view

--- projects/blog/db/seeds.rb
+++ projects/blog/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.delete_all
+
+Post.create!(:title => "Test", :body => "Lorem Ipsum" )
